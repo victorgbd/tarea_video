0a8c71054a71124b8667f4bf34f79e699740c0d2
victor jose <victorjosegonzalez9@gmail.com>
2023-12-26 16:24:33 -0400

    initial

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tarea video',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Tarea Video'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
+  @override
+  void initState() {
+    super.initState();
+
+    _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(
+        'https://assets.mixkit.co/videos/preview/mixkit-spinning-around-the-earth-29351-large.mp4'));
+    _chewieController = ChewieController(
+        autoPlay: true,
+        looping: true,
+        videoPlayerController: _videoPlayerController,
+        aspectRatio: 16 / 9);
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Chewie(controller: _chewieController)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
